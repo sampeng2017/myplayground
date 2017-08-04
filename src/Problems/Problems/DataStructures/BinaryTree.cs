5e8a20b2a2cf7b4044a8d3961974acdee65c7f50
Sam Peng <shepeng@microsoft.com>
2017-08-04 15:09:36 -0700

    stuff

---
+++ src/Problems/Problems/DataStructures/BinaryTree.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.DataStructures
+{
+    public class BinaryTreeNode<T> 
+    {
+        public T Value { get; set; }
+        public BinaryTreeNode<T> LeftChild { get; set; }
+        public BinaryTreeNode<T> RightChild { get; set; }
+    }
+}
---
+++ src/Problems/Problems/OldProblems.cs
@@ -0,0 +1,16 @@
+﻿using Problems.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public static class OldProblems
+    {
+        public static void FlipBinaryTree(BinaryTreeNode<int> tree)
+        {
+        }
+    }
+}
