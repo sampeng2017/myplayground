39643818a13e3985859009f57e8788c3aa0c78cc
Sam Peng <shepeng@microsoft.com>
2017-08-06 16:21:41 -0700

    Add CanPlaceFlower

---
+++ src/Problems/Problems/LeetCoder.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems
+{
+    public static class LeetCoder
+    {
+        // https://leetcode.com/problems/can-place-flowers/description/
+        public static bool CanPlaceFlower(int[] flowerBed, int n)
+        {
+            // set init val to 1 because the previous value of flowerBed[0] is considered as 0
+            int cntConsecutiveZero = 1;
+            int slotCount = 0;
+            for (int i = 0; i < flowerBed.Length; i++)
+            {
+                if (flowerBed[i] == 1)
+                {
+                    cntConsecutiveZero = 0;
+                    continue;
+                }
+                cntConsecutiveZero++;
+                if (cntConsecutiveZero == 3)
+                {
+                    cntConsecutiveZero = 1;
+                    slotCount++;
+                    if (slotCount == n)
+                    {
+                        return true;
+                    }
+                }
+
+                if (i == flowerBed.Length - 1 && cntConsecutiveZero == 2)
+                {
+                    return ++slotCount == n;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/Problems/Tests/LeetCoderTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.DataStructures;
+using Problems;
+using FluentAssertions;
+
+namespace Tests
+{
+    [TestClass]
+    public class LeetCoderTests
+    {
+        [TestMethod]
+        public void CanPlaceFlower()
+        {
+            int[] testBed1 = new int[] { 1, 0, 0 };
+            LeetCoder.CanPlaceFlower(testBed1, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed1, 2).Should().BeFalse();
+
+            int[] testBed2 = new int[] { 1, 0, 0, 0, 1 };
+            LeetCoder.CanPlaceFlower(testBed2, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed2, 2).Should().BeFalse();
+            LeetCoder.CanPlaceFlower(testBed2, 3).Should().BeFalse();
+
+            int[] testBed3 = new int[] { 0, 0, 0, 0, 0 };
+            LeetCoder.CanPlaceFlower(testBed3, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed3, 2).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed3, 3).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed3, 4).Should().BeFalse();
+
+            int[] testBed4 = new int[] { 1, 1, 1, 1, 1 };
+            LeetCoder.CanPlaceFlower(testBed4, 1).Should().BeFalse();
+
+            int[] testBed5 = new int[] { 1, 0, 0, 0, 0, 1, 0, 0 };
+            LeetCoder.CanPlaceFlower(testBed5, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed5, 2).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed5, 3).Should().BeFalse();
+
+            int[] testBed6 = new int[] { 0 };
+            LeetCoder.CanPlaceFlower(testBed6, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed6, 2).Should().BeFalse();
+
+            int[] testBed7 = new int[] { 0, 0, 0, 0, 0, 1, 0, 1 };
+            LeetCoder.CanPlaceFlower(testBed7, 1).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed7, 2).Should().BeTrue();
+            LeetCoder.CanPlaceFlower(testBed7, 3).Should().BeFalse();
+        }
+    }
+}
