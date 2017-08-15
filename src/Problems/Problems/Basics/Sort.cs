3c94f97f72edc12df6c5e0cab2774f3a119f3f98
Sam Peng <shepeng@microsoft.com>
2017-08-14 18:07:14 -0700

    Add sorts, merge sort not done yet

---
+++ src/Problems/Problems/Basics/Sort.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problems.Basics
+{
+    public static class Sort
+    {
+        public static void InsertionSort(int[] ary)
+        {
+            if (ary == null || ary.Length == 1)
+                return;
+
+            for (int i = 1; i < ary.Length; i++)
+            {
+                int key = ary[i];
+                int j = i - 1;
+                while (j >= 0 && ary[j] > key)
+                {
+                    ary[j + 1] = ary[j];
+                    j--;
+                }
+                ary[j + 1] = key;
+            }
+        }
+
+        public static void MergeSort(int[] ary)
+        {
+        }
+
+        private static void Merge(int[] ary, int p, int q, int r)
+        {
+            int n1 = q - p + 1;
+            int n2 = r - q;
+            int[] left = new int[n1 + 1];
+            left[n1] = int.MaxValue; // sentinel
+
+            int[] right = new int[n2 + 1];
+            right[n2] = int.MaxValue;
+
+            for (int ii = 0; ii < n1; ii++)
+            {
+                left[ii] = ary[p + ii]; 
+            }
+            for (int ii = 0; ii < n2; ii++)
+            {
+                right[ii] = ary[q + ii + 1];
+            }
+
+            int i = 0;
+            int j = 0;
+            for (int k = p; k < r; k++)
+            {
+                if (left[i] <= right[j])
+                {
+                    ary[k] = left[i];
+                    i++;
+                }
+                else
+                {
+                    ary[k] = right[j];
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ src/Problems/Tests/BasicsTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.DataStructures;
+using Problems;
+using FluentAssertions;
+using Problems.Basics;
+
+namespace Tests
+{
+    [TestClass]
+    public class BasicsTests
+    {
+        [TestMethod]
+        public void SortTest()
+        {
+            int[] ary = GetRandomArray(100);
+            Action<int[]> sort = Sort.InsertionSort;
+            sort(ary);
+            IsSorted(ary).Should().Be(true);
+        }
+        private int[] GetRandomArray(int cnt)
+        {
+            var ary = new int[cnt];
+            var rand = new Random(DateTime.Now.Millisecond);
+            for (int i = 0; i < cnt; i++)
+            {
+                ary[i] = rand.Next();
+            }
+            return ary;
+        }
+
+        private bool IsSorted(int[] ary)
+        {
+            int previous = int.MinValue;
+            for (int i = 0; i < ary.Length; i++)
+            {
+                if (ary[i]< previous)
+                {
+                    return false;
+                }
+                previous = ary[i];
+            }
+            return true;
+        }
+    }
+}
