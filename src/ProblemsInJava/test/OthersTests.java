5f584a5efb7f2ffb3fe2bfc4178617b3f5b737e8
Sam Peng <shepeng@microsoft.com>
2017-10-04 00:01:21 -0700

    stuff

--- src/ProblemsInJava/test/OthersTests.java
+++ src/ProblemsInJava/test/OthersTests.java
@@ -18,44 +18,52 @@ import static org.junit.Assert.*;
 public class OthersTests {
 
     @Test
-    public void BuildOrderTest() {
+    public void BuildOrderTest() throws Exception {
+
+        Iterable<String> buildOrder = null;
+        List<String> list = new ArrayList<String>();
+
+        // null input
+        buildOrder = BuildResolver.GetBuildOrder(null);
+        assertEquals(null, buildOrder);
+
+        // single module
+        ArrayList<Pair<String, Iterable<String>>> moduleWithDependencies = new ArrayList<Pair<String, Iterable<String>>>();
+        Pair<String, Iterable<String>> c = new Pair<String, Iterable<String>>("c", new ArrayList<String>());
+        moduleWithDependencies.add(c);
+
+        buildOrder = BuildResolver.GetBuildOrder(moduleWithDependencies);
+        buildOrder.forEach(list::add);
+        assertArrayEquals(list.toArray(), new String[]{"c"});
+        list.clear();
+
+        // multiple modules
         // a -> b, c
         // b -> c, d
         // d -> e
         // f -> g
-        ArrayList<Pair<String, Iterable<String>>> moduleWithDependencies = new ArrayList<Pair<String, Iterable<String>>>();
-        Pair<String, Iterable<String>> c = new Pair<String, Iterable<String>>("c", new ArrayList<String>());
         Pair<String, Iterable<String>> f = new Pair<String, Iterable<String>>("f", new ArrayList<String>(asList("g")));
         Pair<String, Iterable<String>> a = new Pair<String, Iterable<String>>("a", new ArrayList<String>(asList("b", "c")));
         Pair<String, Iterable<String>> b = new Pair<String, Iterable<String>>("b", new ArrayList<String>(asList("c", "d")));
         Pair<String, Iterable<String>> d = new Pair<String, Iterable<String>>("d", new ArrayList<String>(asList("e")));
-        moduleWithDependencies.add(c);
         moduleWithDependencies.add(f);
         moduleWithDependencies.add(a);
         moduleWithDependencies.add(b);
         moduleWithDependencies.add(d);
 
-        Iterable<String> buildOrder = null;
-        try {
-
-            buildOrder = BuildResolver.GetBuildOrder(moduleWithDependencies);
-        }
-        catch (Exception ex) {
-            fail("unexpected");
-        }
-        List<String> list = new ArrayList<String>();
+        buildOrder = BuildResolver.GetBuildOrder(moduleWithDependencies);
         buildOrder.forEach(list::add);
-            
-        org.junit.Assert.assertArrayEquals(list.toArray(), new String[] {"e", "d", "c", "b", "a", "g", "f"});
-        
-        // circular dependecy scenario. e -> b
+
+        assertArrayEquals(list.toArray(), new String[]{"e", "d", "c", "b", "a", "g", "f"});
+        list.clear();
+
+        // circular dependecy e -> b
         moduleWithDependencies.add(new Pair<String, Iterable<String>>("e", new ArrayList<String>(asList("b"))));
-         try {
+        try {
             buildOrder = BuildResolver.GetBuildOrder(moduleWithDependencies);
             fail("expected exception not raised");
-        }
-        catch (Exception ex) {
-            org.junit.Assert.assertEquals(ex.getMessage(), "Circular path detected: e -> b");
+        } catch (Exception ex) {
+            assertEquals(ex.getMessage(), "Circular path detected: e -> b");
         }
     }
 }
