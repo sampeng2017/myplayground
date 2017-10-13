8c9ceaf307b5d1b80f9de6736bd79117922161a3
Sam Peng <shepeng@microsoft.com>
2017-10-13 12:26:32 -0700

    Visual change

--- src/IChing/IChingWinForm/Form1.Designer.cs
+++ src/IChing/IChingWinForm/Form1.Designer.cs
@@ -45,21 +45,23 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBoxContent.BackColor = System.Drawing.SystemColors.Info;
             this.textBoxContent.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.textBoxContent.Location = new System.Drawing.Point(12, 131);
+            this.textBoxContent.Location = new System.Drawing.Point(11, 72);
+            this.textBoxContent.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxContent.Multiline = true;
             this.textBoxContent.Name = "textBoxContent";
             this.textBoxContent.ReadOnly = true;
             this.textBoxContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBoxContent.Size = new System.Drawing.Size(1312, 704);
+            this.textBoxContent.Size = new System.Drawing.Size(777, 390);
             this.textBoxContent.TabIndex = 0;
             // 
             // labelName
             // 
             this.labelName.AutoSize = true;
             this.labelName.Font = new System.Drawing.Font("SimSun", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelName.Location = new System.Drawing.Point(163, 60);
+            this.labelName.Location = new System.Drawing.Point(93, 33);
+            this.labelName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelName.Name = "labelName";
-            this.labelName.Size = new System.Drawing.Size(150, 42);
+            this.labelName.Size = new System.Drawing.Size(88, 24);
             this.labelName.TabIndex = 1;
             this.labelName.Text = "label1";
             // 
@@ -68,18 +70,20 @@
             this.labelDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.labelDate.AutoSize = true;
             this.labelDate.Font = new System.Drawing.Font("SimSun", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labelDate.Location = new System.Drawing.Point(808, 60);
+            this.labelDate.Location = new System.Drawing.Point(462, 33);
+            this.labelDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.labelDate.Name = "labelDate";
-            this.labelDate.Size = new System.Drawing.Size(123, 36);
+            this.labelDate.Size = new System.Drawing.Size(69, 20);
             this.labelDate.TabIndex = 2;
             this.labelDate.Text = "label1";
             this.labelDate.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // pictureBoxGua
             // 
-            this.pictureBoxGua.Location = new System.Drawing.Point(36, 23);
+            this.pictureBoxGua.Location = new System.Drawing.Point(21, 13);
+            this.pictureBoxGua.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBoxGua.Name = "pictureBoxGua";
-            this.pictureBoxGua.Size = new System.Drawing.Size(100, 102);
+            this.pictureBoxGua.Size = new System.Drawing.Size(57, 56);
             this.pictureBoxGua.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBoxGua.TabIndex = 4;
             this.pictureBoxGua.TabStop = false;
@@ -89,9 +93,10 @@
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::IChingWinForm.Properties.Resources.yijing;
-            this.pictureBox1.Location = new System.Drawing.Point(706, 36);
+            this.pictureBox1.Location = new System.Drawing.Point(403, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(96, 89);
+            this.pictureBox1.Size = new System.Drawing.Size(55, 49);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
@@ -99,15 +104,16 @@
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1399, 847);
+            this.ClientSize = new System.Drawing.Size(799, 467);
             this.Controls.Add(this.pictureBoxGua);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.labelDate);
             this.Controls.Add(this.labelName);
             this.Controls.Add(this.textBoxContent);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FormMain";
             this.Text = "易經占蔔";
             this.Load += new System.EventHandler(this.Form1_Load);
